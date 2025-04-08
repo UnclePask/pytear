db8a2105b9cd98b51deadf6819241cd2f998470f
UnclePask <brezza_occhiale_0c@icloud.com>
2025-04-09 00:05:04 +0200

    Update main.py

--- main/main.py
+++ main/main.py
@@ -3,6 +3,7 @@ Created on 21 dic 2024
 
 @author: pasquale
 '''
+import convert as make
 import step_1 as at1_do
 import step_2 as at2_do
 import step_3 as at3_do
@@ -14,16 +15,16 @@ def start_pipeline(FilePath):
     print('\n\nCiao, questa è PyTeAR (alpha 0)!\nthe routine transformation pipeline to identify the propaganda content \nin the speechs and articles\n\n')
     node1 = at1_do.step_1(FilePath, True)
     table1 = node1.data_scout()
-#    table1.to_csv('../speech-1_2.csv', sep=';')
+    table1.to_csv('../pytear/outputfile/speech-1_2.csv', sep=';')
     print('\n')
     node2 = at2_do.step_2(table1)
     table2 = node2.text_analysis()
-#   table2.to_csv('../speech-3_4.csv', sep=';')
+    table2.to_csv('../pytear/outputfile/speech-3_4.csv', sep=';')
     print('\n')
     node3 = at3_do.step_3(table2)
     table3 = node3.propaganda_detection()
-    table3.to_csv('../outputFile/speech-b.csv', sep=';')
-    new_file_path = Path('../outputFile/speech-b.csv')
+    table3.to_csv('../pytear/outputFile/speech-b.csv', sep=';')
+    new_file_path = Path('../pytear/outputFile/speech-b.csv')
     if new_file_path.exists():
         print("\nThe output file ../speech-b.csv generated\n\n")
         return 0
@@ -34,7 +35,7 @@ def start_pipeline(FilePath):
     
 def main():
 #    anagraphPath = '../inputFile/anagraphic.json'
-    inputFile = '../inputFile/speech-a.tsv'
+    inputFile = make.convert.getPath('input')
 #    exit_val = start_pipeline(inputFile, anagraphPath)
     exit_val = start_pipeline(inputFile)
     return exit_val
