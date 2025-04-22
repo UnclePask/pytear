3f8192ada45964c581cef279ad06fbb93232e118
UnclePask <brezza_occhiale_0c@icloud.com>
2025-04-23 00:46:29 +0200

    Update BERT_arch.py

--- main/bert_ml/BERT_arch.py
+++ main/bert_ml/BERT_arch.py
@@ -6,9 +6,6 @@ Created on 4 gen 2025
 import torch.nn as nn
 
 class BERT_Arch(nn.Module):
-    '''
-    Architettura del modello Home Made UnclePask
-    '''
     def __init__(self, bert):
         super(BERT_Arch, self).__init__()
         self.bert = bert
@@ -19,10 +16,7 @@ class BERT_Arch(nn.Module):
         self.softmax = nn.LogSoftmax(dim=1)  
     
     def forward(self, sent_id, mask):           
-        '''
-        il batch contiene la rappresentazione lineare (2 pretrained)
-        '''
-        cls_hs = self.bert(sent_id, attention_mask=mask)['pooler_output'] #chiamata iterativa prodotto scalare per i vettori dei pesi (chiave, query, valori)
+        cls_hs = self.bert(sent_id, attention_mask=mask)['pooler_output']
         nex = self.fc1(cls_hs)
         nex = self.relu(nex)
         nex = self.dropout(nex)
