0308e3d713689527ca41ae071e1b9b30ec02c31b
UnclePask <brezza_occhiale_0c@icloud.com>
2025-04-22 22:57:54 +0200

    Update BERT_arch.py

--- main/bert_ml/BERT_arch.py
+++ main/bert_ml/BERT_arch.py
@@ -12,22 +12,20 @@ class BERT_Arch(nn.Module):
     def __init__(self, bert):
         super(BERT_Arch, self).__init__()
         self.bert = bert
-        self.dropout = nn.Dropout(0.1)       
-        self.relu =  nn.ReLU()               
-        self.fc1 = nn.Linear(768,512)        
-        self.fc2 = nn.Linear(512,2)          
+        self.dropout = nn.Dropout(0.1)
+        self.relu =  nn.ReLU()                  
+        self.fc1 = nn.Linear(32,16)         
+        self.fc2 = nn.Linear(16,2)        
         self.softmax = nn.LogSoftmax(dim=1)  
     
     def forward(self, sent_id, mask):           
         '''
         il batch contiene la rappresentazione lineare (2 pretrained)
         '''
-        cls_hs = self.bert(sent_id, attention_mask=mask)['pooler_output']
+        cls_hs = self.bert(sent_id, attention_mask=mask)['pooler_output'] #chiamata iterativa prodotto scalare per i vettori dei pesi (chiave, query, valori)
         nex = self.fc1(cls_hs)
         nex = self.relu(nex)
         nex = self.dropout(nex)
         nex = self.fc2(nex)                       
         nex = self.softmax(nex)                   
         return nex
-
-    
